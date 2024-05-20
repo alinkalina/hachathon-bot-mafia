829939e93b0ee16a6b31898917c0c9e35571d34b
NickyBricky <buninkola16@gmail.com>
2024-05-20 23:54:13 +0300

    добавляем файл с константами

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from telebot.types import BotCommand
+
+load_dotenv()
+
+# Бот
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+LINK_TO_BOT = "https://t.me/hackathon_mafia_bot"
+COMMANDS = [BotCommand(command="start", description="Запуск бота"),
+            BotCommand(command="rules", description="Подробные правила игры в мафию")]
+
+
+# Игра
+
+SHORT_RULES = "Какие-то правила"  # TODO дописать короткие правила
+FULL_RULES = "Какие-то правила"  # TODO дописать подробные правила
