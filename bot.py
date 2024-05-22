7c4ec28e7fc772105bd4d32576da49766668655e
NickyBricky <buninkola16@gmail.com>
2024-05-22 14:13:40 +0300

    Добавлены функции дневной фазы в бота

--- bot.py
+++ bot.py
@@ -72,6 +72,7 @@ def ready_handler(call):
     if not check_user_exists(user_id):
         return_to_private_btn = InlineKeyboardButton(text="Чат с ботом", url=LINK_TO_BOT)
         keyboard = InlineKeyboardMarkup().add(return_to_private_btn)
+
         bot.send_message(call.message.chat.id, f"{call.from_user.username}, "
                                                f"пожалуйста, напишите /start в чате со мной.",
                          reply_markup=keyboard)
@@ -127,6 +128,127 @@ def start_game_handler(message):
 
         sent_message = bot.send_message(c_id, "Нажмите на кнопку, когда будете готовы!", reply_markup=markup)
         start_game_timer(sent_message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data is int)
+def process_user_votes(call):
+    c_id = call.message.chat.id
+
+    voted_user_id = call.from_user.id
+    chosen_user_id = call.data
+
+    # add_user_choice(voted_user_id, chosen_user_id) TODO Функция добавления выбора игрока по его айди
+
+    link_to_group = get_group_link(voted_user_id)
+
+    # создаем кнопку для перехода в группу
+    return_to_group_btn = InlineKeyboardButton(text="Вернуться в группу", url=link_to_group)
+    return_to_group_keyboard = InlineKeyboardMarkup().add(return_to_group_btn)
+
+    chosen_user_name = bot.get_chat_member(c_id, chosen_user_id)
+    bot.edit_message_text(text=f"Ваш выбор: {chosen_user_name}", reply_markup=return_to_group_keyboard)
+
+    voted_user_name = bot.get_chat_member(c_id, voted_user_id)
+    bot.send_message(c_id, f"Игрок {voted_user_name} сделал свой выбор.")
+
+
+def start_voting_timer(message, delay=30):
+    def timer_func():
+        c_id = message.chat.id
+
+        bot.send_message(c_id, "Голосование завершено!")
+
+        # получаем статус и user_id
+        # voting_result = get_voting_results() TODO Функция подсчета голосов на дневном голосовании
+        voting_result = []  # временный вариант
+
+        if not voting_result[0]:
+            bot.send_message(c_id, "Жители решили никого не убивать сегодня.")
+
+        else:
+            killed_user_id = voting_result[1]
+
+            killed_user_name = bot.get_chat_member(c_id, killed_user_id).user.username
+
+            # killed_user_role = get_user_data().role TODO Добавить функцию получения данных об игроке
+            killed_user_role = "какая-то роль"
+
+            bot.send_message(c_id, f"Сегодня был изгнан игрок {killed_user_name} с ролью {killed_user_role}")
+
+            # change_user_info(voting_result, "killed", data=1) TODO добавить функцию изменения информации об игроке
+
+            # make_night_stage() TODO снова переходим в функцию начала ночи
+
+    threading.Timer(delay, timer_func).start()
+
+
+def make_voting(message, alive_users):
+    alive_players_keyboard = InlineKeyboardMarkup()
+
+    for user_info in alive_users:
+        btn = InlineKeyboardButton(text=user_info[1], callback_data=user_info[0])
+        alive_players_keyboard.add(btn)
+
+    return_to_private_btn = InlineKeyboardButton(text="Чат с ботом", url=LINK_TO_BOT)
+    return_to_private_keyboard = InlineKeyboardMarkup().add(return_to_private_btn)
+
+    bot.send_message(message.chat.id, "Настало время голосования! Всех живых игроков прошу перейти в чат со мной.",
+                     reply_markup=return_to_private_keyboard)
+
+    for user_info in alive_users:
+        bot.send_message(user_info[0], "Пришло время кого-нибудь изгнать! "
+                                       "У вас есть 30 секунд на то, чтобы сделать выбор",
+                         reply_markup=alive_players_keyboard)
+
+    start_voting_timer(message)
+
+
+def start_discussion_timer(message, alive_users, delay=180):
+    def timer_func():
+        make_voting(message, alive_users)
+
+    threading.Timer(delay, timer_func).start()
+
+
+def make_day_stage(message, killed_user_list: list):
+    c_id = message.chat.id
+    is_user_killed = killed_user_list[0]
+
+    if not is_user_killed:
+        bot.send_message(c_id, "Кажется, мафия не смогла договориться и этой ночью никто не был убит.")
+
+    else:
+        killed_user_id = killed_user_list[1]
+
+        killed_user_name = bot.get_chat_member(c_id, killed_user_id).user.username
+
+        bot.send_message(c_id, f"Этой ночью был зверски убит игрок по имени {killed_user_name}")
+
+        # change_user_info(killed_user_id, "killed", data=1) TODO добавить функцию изменения информации об игроке
+
+    # alive_users_ids = get_alive_users() TODO Функция для получения списка из телеграм айди живых игроков
+    alive_user_ids = []  # временный вариант
+    alive_user_names = []
+
+    for user_id in alive_user_ids:
+        user_name = bot.get_chat_member(message.chat.id, user_id)
+        alive_user_names.append(user_name)
+
+    text = "Этой ночью остались в живых:\n\n"
+
+    for i, user_name in enumerate(alive_user_names):
+        text += f"{i}. {user_name}\n"
+
+    bot.send_message(c_id, text)
+
+    bot.send_message(c_id, "Настало время для обсуждения! Даю вам 3 минуты!")
+
+    alive_users = []
+
+    for i in range(len(alive_user_ids)):
+        alive_users.append([alive_user_ids[i], alive_user_names[i]])
+
+    start_discussion_timer(message, alive_users)
 
 
 if __name__ == "__main__":
