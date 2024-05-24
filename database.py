365ba66921f449b9dbc58f61e0e862759ee38ec6
alinkalina <alinkameshch@yandex.ru>
2024-05-25 00:29:45 +0300

    отрефакторила database.py

--- database.py
+++ database.py
@@ -2,12 +2,37 @@ import sqlite3
 from config import ROLES
 
 
+# НАСТРОЙКА БД, ЧАСТО ИСПОЛЬЗУЕМЫЕ ФУНКЦИИ
+
+# открытие базы
 def open_db():
     con = sqlite3.connect('sqlite.db', check_same_thread=False)
     cur = con.cursor()
     return con, cur
 
 
+#  ДОЛЖНА использоваться только в этом файле для запросов UPDATE, INSERT и DELETE
+def change_db(sql: str):
+    connection, cursor = open_db()
+    cursor.execute(sql)
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+
+# ДОЛЖНА использоваться только в этом файле для запросов SELECT
+def get_from_db(sql: str) -> list[tuple]:
+    connection, cursor = open_db()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
+
+
+# СОЗДАНИЕ ТАБЛИЦ, ДОБАВЛЕНИЕ РОЛЕЙ
+
+# создание таблиц (вызывается каждый раз при запуске проекта)
 def create_tables():
     connection, cursor = open_db()
 
@@ -66,31 +91,12 @@ def create_tables():
     connection.close()
 
 
-# эта функция должна использоваться только в этом файле для запросов UPDATE, INSERT и DELETE
-def change_db(sql: str):
-    connection, cursor = open_db()
-    cursor.execute(sql)
-    cursor.close()
-    connection.commit()
-    connection.close()
-
-
-# эта функция должна использоваться только в этом файле для запросов SELECT
-def get_from_db(sql: str) -> list[tuple]:
-    connection, cursor = open_db()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return result
-
-
-# функция вызывается каждый раз при запуске проекта и добавляет в БД новые роли, если они появились
+# добавляет в БД новые роли, если они появились (вызывается каждый раз при запуске проекта)
 def update_roles():
     for role in ROLES:
         connection, cursor = open_db()
+        sql = f'INSERT INTO roles (name) VALUES ("{role}");'
         try:
-            sql = f'INSERT INTO roles (name) VALUES ("{role}");'
             cursor.execute(sql)
         except sqlite3.IntegrityError:
             pass
@@ -100,7 +106,9 @@ def update_roles():
             connection.close()
 
 
-# добавляет нового пользователя в таблицу users если его там ещё нет
+# ПОЛЬЗОВАТЕЛИ
+
+# добавляет нового пользователя в таблицу users, если его там ещё нет
 def add_user(chat_id: int):
     connection, cursor = open_db()
     sql = f'INSERT INTO users (chat_id) VALUES ({chat_id});'
@@ -122,60 +130,6 @@ def check_user_exists(chat_id: int) -> bool:
     return bool(result)
 
 
-# добавляет новую группу в таблицу groups если её там ещё нет
-def add_group(group_chat_id: int):
-    connection, cursor = open_db()
-    sql = f'INSERT INTO groups (group_chat_id) VALUES ({group_chat_id});'
-    try:
-        cursor.execute(sql)
-    except sqlite3.IntegrityError:
-        pass
-    finally:
-        connection.commit()
-        cursor.close()
-        connection.close()
-
-
-# возвращает True, если группа сейчас играет, False - если нет
-def is_group_playing(group_chat_id: int) -> bool:
-    sql = f'SELECT is_playing FROM groups WHERE group_chat_id = {group_chat_id};'
-    result = get_from_db(sql)
-
-    if not result:
-        return False
-    return bool(result[0][0])
-
-
-# возвращает текущее кол-во сессий группы
-def get_group_current_session(group_chat_id: int) -> int:
-    sql = f'SELECT session FROM groups WHERE group_chat_id = {group_chat_id};'
-    result = get_from_db(sql)
-    return result[0][0]
-
-
-# переводит статус группы is_playing в нужный, state = 0 - не играет, 1 - играет
-def change_group_state(group_chat_id: int, state: int):
-    sql = f'UPDATE groups SET is_playing = {state} WHERE group_chat_id = {group_chat_id};'
-    change_db(sql)
-
-
-# увеличивает сессию на 1
-def increase_session(group_chat_id: int):
-    current_session = get_group_current_session(group_chat_id)
-    sql = f'UPDATE groups SET session = {current_session + 1} WHERE group_chat_id = {group_chat_id};'
-    change_db(sql)
-
-
-# должна использоваться только в этом файле
-def get_one_by_other(param_1: str, param_2: str, value: int | str, table_name: str) -> int | str:
-    if table_name == 'roles' and param_1 == 'id':
-        sql = f'SELECT {param_1} FROM {table_name} WHERE {param_2} = "{value}";'
-    else:
-        sql = f'SELECT {param_1} FROM {table_name} WHERE {param_2} = {value};'
-    result = get_from_db(sql)
-    return result[0][0]
-
-
 # возвращает True, если юзер сейчас играет, False - если нет
 def is_user_playing(user_chat_id: int) -> bool:
     user_id = get_one_by_other('id', 'chat_id', user_chat_id, table_name='users')
@@ -190,11 +144,79 @@ def is_user_playing(user_chat_id: int) -> bool:
     return False
 
 
+# ГРУППЫ
+
+# добавляет новую группу в таблицу groups, если её там ещё нет
+def add_group(group_chat_id: int):
+    connection, cursor = open_db()
+    sql = f'INSERT INTO groups (group_chat_id) VALUES ({group_chat_id});'
+    try:
+        cursor.execute(sql)
+    except sqlite3.IntegrityError:
+        pass
+    finally:
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+
+# переводит статус группы is_playing в нужный, state = 0 - не играет, 1 - играет
+def change_group_state(group_chat_id: int, state: int):
+    sql = f'UPDATE groups SET is_playing = {state} WHERE group_chat_id = {group_chat_id};'
+    change_db(sql)
+
+
+# возвращает текущее кол-во сессий группы
+def get_group_current_session(group_chat_id: int) -> int:
+    sql = f'SELECT session FROM groups WHERE group_chat_id = {group_chat_id};'
+    result = get_from_db(sql)
+    return result[0][0]
+
+
+# увеличивает сессию на 1
+def increase_session(group_chat_id: int):
+    current_session = get_group_current_session(group_chat_id)
+    sql = f'UPDATE groups SET session = {current_session + 1} WHERE group_chat_id = {group_chat_id};'
+    change_db(sql)
+
+
+# возвращает True, если группа сейчас играет, False - если нет
+def is_group_playing(group_chat_id: int) -> bool:
+    sql = f'SELECT is_playing FROM groups WHERE group_chat_id = {group_chat_id};'
+    result = get_from_db(sql)
+    if not result:
+        return False
+    return bool(result[0][0])
+
+
+# ЕЩЁ ЛОКАЛЬНЫЕ ФУНКЦИИ
+
+# ДОЛЖНА использоваться только в этом файле
+def get_one_by_other(param_1: str, param_2: str, value: int | str, table_name: str) -> int | str:
+    if table_name == 'roles' and param_1 == 'id':
+        sql = f'SELECT {param_1} FROM {table_name} WHERE {param_2} = "{value}";'
+    else:
+        sql = f'SELECT {param_1} FROM {table_name} WHERE {param_2} = {value};'
+    result = get_from_db(sql)
+    return result[0][0]
+
+
+# переводит id в результат, который нужно вернуть в bot.py, используется только в этом файле
+def transform_result(result: int, param: str) -> str | int:
+    if param == 'role':
+        result = get_one_by_other('name', 'id', result, table_name='roles')
+    elif param == 'choice':
+        result = get_one_by_other('chat_id', 'id', result, table_name='users')
+    return result
+
+
+# ИГРА
+
 # добавляет пользователя в таблицу games
 def add_user_to_games(group_chat_id: int, user_chat_id: int):
     group_id = get_one_by_other('id', 'group_chat_id', group_chat_id, table_name='groups')
-    user_id = get_one_by_other('id', 'chat_id', user_chat_id, table_name='users')
     session = get_group_current_session(group_chat_id)
+    user_id = get_one_by_other('id', 'chat_id', user_chat_id, table_name='users')
     sql = f'INSERT INTO games (group_id, session, user_id) VALUES ({group_id}, {session}, {user_id});'
     change_db(sql)
 
@@ -226,7 +248,7 @@ def get_players_list(group_chat_id: int) -> list[int]:
     return chat_ids
 
 
-# для изменения данных в games, param - название колонки, data - данные (роль текстом, chat_id юзера или 0/1 (жив/убит))
+# меняет данные юзера в games, param - название колонки, data - данные (роль текстом, chat_id юзера или 0/1 (жив/убит))
 def update_user_data(user_chat_id: int, group_chat_id: int, param: str, data: str | int | None):
     user_id = get_one_by_other('id', 'chat_id', user_chat_id, table_name='users')
     group_id = get_one_by_other('id', 'group_chat_id', group_chat_id, table_name='groups')
@@ -242,7 +264,7 @@ def update_user_data(user_chat_id: int, group_chat_id: int, param: str, data: st
     change_db(sql)
 
 
-# получение данных из games, param - название колонки, возвращает роль текстом, chat_id юзера или 0/1 (жив/убит)
+# получает данные юзера из games, param - название колонки, возвращает роль текстом, chat_id юзера или 0/1 (жив/убит)
 def get_user_data(user_chat_id: int, group_chat_id: int, param: str) -> str | int | None:
     user_id = get_one_by_other('id', 'chat_id', user_chat_id, table_name='users')
     group_id = get_one_by_other('id', 'group_chat_id', group_chat_id, table_name='groups')
@@ -254,16 +276,7 @@ def get_user_data(user_chat_id: int, group_chat_id: int, param: str) -> str | in
     return result
 
 
-# переводит id в результат, который нужно вернуть в bot.py, используется только в этом файле
-def transform_result(result: int, param: str) -> str | int:
-    if param == 'role':
-        result = get_one_by_other('name', 'id', result, table_name='roles')
-    elif param == 'choice':
-        result = get_one_by_other('chat_id', 'id', result, table_name='users')
-    return result
-
-
-# список живых игроков
+# получение списка живых игроков
 def get_alive_users(group_chat_id: int) -> list[int]:
     chat_ids = get_players_list(group_chat_id)
     alive_users = []
@@ -279,8 +292,8 @@ def get_users_with_role(group_chat_id: int, role: str) -> list[int]:
     chat_ids = get_alive_users(group_chat_id)
     users_with_role = []
     for chat_id in chat_ids:
-        user_role_id = get_user_data(chat_id, group_chat_id, 'role')
-        if user_role_id == role:
+        user_role = get_user_data(chat_id, group_chat_id, 'role')
+        if user_role == role:
             users_with_role.append(chat_id)
     return users_with_role
 
